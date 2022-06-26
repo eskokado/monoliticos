42d7d45eac5a6e73b13aa9539eb0c52e0dea585b
eskokado <eskokado@gmail.com>
2022-06-26 10:52:18 -0300

    test: add client in clientrepository

--- src/modules/client-adm/repository/client.repository.spec.ts
+++ src/modules/client-adm/repository/client.repository.spec.ts
@@ -1,4 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
+import Id from "../../@shared/domain/value-object/id.value-object";
+import Client from "../domain/client.entity";
 import ClientModel from "./client.model";
 import ClientRepository from "./client.repository";
 
@@ -45,4 +47,28 @@ describe("ClientRepository test", () => {
     expect(result.email).toEqual(client.email);
     expect(result.address).toEqual(client.address);    
   });
+
+  it("should create a client", async () => {
+    const clientProps = {
+      id: new Id("1"),
+      name: "Client 1",
+      email: "email@client1.com",
+      address: "Address Client1"
+    }
+
+    const client = new Client(clientProps);
+
+    const clientRepository = new ClientRepository();
+    await clientRepository.add(client);
+
+    const clientDb = await ClientModel.findOne({
+      where: { id: clientProps.id.id },
+    });
+
+    expect(clientProps.id.id).toEqual(clientDb.id);
+    expect(clientProps.name).toEqual(clientDb.name);
+    expect(clientProps.email).toEqual(clientDb.email);
+    expect(clientProps.address).toEqual(clientDb.address);
+  });
+
 });
