dd0471401be285128f763c3a83c841a351a1214a
eskokado <eskokado@gmail.com>
2022-06-26 10:52:03 -0300

    feat: add client in clientrepository

--- src/modules/client-adm/repository/client.repository.ts
+++ src/modules/client-adm/repository/client.repository.ts
@@ -1,13 +1,20 @@
 import Id from "../../@shared/domain/value-object/id.value-object";
 import Client from "../domain/client.entity";
-import clientEntity from "../domain/client.entity";
 import ClientGateway from "../gateway/client.gateway";
 import ClientModel from "./client.model";
 
 export default class ClientRepository implements ClientGateway {
-  add(client: clientEntity): Promise<void> {
-    throw new Error("Method not implemented.");
+  async add(client: Client): Promise<void> {
+    await ClientModel.create({
+      id: client.id.id,
+      name: client.name,
+      email: client.email,
+      address: client.address,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    });
   }
+
   async find(id: string): Promise<Client> {
     const client = await ClientModel.findOne({where: {id: id}});
 
