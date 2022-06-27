3378c96e3831dac00f04d3647974a3f873f54570
eskokado <eskokado@gmail.com>
2022-06-27 07:57:11 -0300

    feat: calls save in transaction repository should returns a transaction

---
+++ src/modules/payment/repository/transaction.repository.ts
@@ -0,0 +1,29 @@
+import Id from "../../@shared/domain/value-object/id.value-object";
+import Transaction from "../domain/transaction";
+import PaymentGateway from "../gateway/payment.gateway";
+import TransactionModel from "./transaction.model";
+
+export default class TransactionRepository implements PaymentGateway {
+  async save(input: Transaction): Promise<Transaction> {
+    const transaction = await TransactionModel.create({
+      id: input.id.id,
+      orderId: input.orderId,
+      amount: input.amount,
+      status: input.status,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    });
+
+    const props = {
+      id: new Id(transaction.id),
+      orderId: transaction.orderId,
+      amount: transaction.amount,
+      status: transaction.status,
+      createdAt: transaction.createdAt,
+      updatedAt: transaction.updatedAt
+    }
+
+    return new Transaction(props);
+  }
+
+}
